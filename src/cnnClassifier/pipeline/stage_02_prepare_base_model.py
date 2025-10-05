96502236423bc6c605306c9a7c7c74bbd48598ac
Ayush Kumar Jha <kumarayushjha123@gmail.com>
2025-10-05 09:41:15 +0530

    Model Trained and evaluated

--- src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/cnnClassifier/pipeline/stage_02_prepare_base_model.py
@@ -15,6 +15,7 @@ class PrepareBaseModelTrainingPipeline:
         prepare_base_model.get_base_model()
         prepare_base_model.update_base_model()
 
+
 if __name__ == "__main__":
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
